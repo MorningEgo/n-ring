822e8c81c5135c1d0288d3251d902599c1f0c9c2
MorningEgo <101182056+MorningEgo@users.noreply.github.com>
2023-09-01 12:04:14 +0000

    ンゴチャレンジのレア演出を追加、調整

--- command_files/challenge.py
+++ command_files/challenge.py
@@ -13,9 +13,12 @@ async def challenge(ctx:I.discord.Interaction):
   
   _ = ''.join(A)
   match = ""
+  ## かんぺき ##
   if _ == "んごりンゴ":
-    match = "\n\n**Foo！！！**"
-    #2000NP
+    match = "\n\n\n\n# _Foo!!!!!!_"
+    # 500 NP
+  
+  ## おしい ##
   elif _ == "ンゴりんご":
     match = "\n\n......？"
   elif _ == "んごりんご":
@@ -24,15 +27,35 @@ async def challenge(ctx:I.discord.Interaction):
     match = "\n\n......？"
   elif _ == "ゴンりごん":
     match = "\n\n\N{Clockwise Rightwards and Leftwards Open Circle Arrows}"
+  elif _ == "んんごごりりンンゴゴ":
+    match = "\n\n_`Glitch`_"
+  
+  ## えぐい ##
   elif _ == "んごりンゴんごりンゴ":
     match = "\n\n\n\n### D  O  U  B  L  E    N    G    O"
+    # 5005 NP
   elif _ == "んごりンゴんごりンゴんごりンゴ":
     match = "\n\n\n\n### T  R  I  P  L  E    N    G    O"
+    # 50505 NP
   elif _ == "んごりンゴんごりンゴんごりンゴんごりンゴ":
     match = "\n\n\n\n# _N  G  O      P  A  R  T  Y_"
+    # 5050505 NP
+
+  ## ンゴ違い ##
+  elif _ == "んご":
+    match = "\n\n......。"
+  
+  ## ンゴ ##
   elif _ == "ンゴ":
-    match = "\n\nンゴ〜"
+    match = "\n\nンゴ～"
+  elif _ == "ンゴンゴ":
+    match = "\n\nンゴッ！？"
+
+  ## りんご ##
   elif _ == "りんご":
     match = "\n\n\N{Green Apple}"
+  elif _ == "ごんり":
+    match = "\n\n\N{Red Apple}"
+  
   
   await ctx.followup.send(f"## {_}！！！{match}")
