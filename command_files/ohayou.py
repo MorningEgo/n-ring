0a0ef424acdf36d1e0fb8541a6cd808fb3af5135
MorningEgo <101182056+MorningEgo@users.noreply.github.com>
2023-10-27 17:43:19 +0000

    苦苦

--- command_files/ohayou.py
+++ command_files/ohayou.py
@@ -1,10 +1,10 @@
 import define as I
-import locale
 
 @I.tree.command(name="おはよう", description="起きます")
 @I.discord.app_commands.guilds(I.discord.Object(id=I.guildid))
-async def challenge(ctx: I.discord.Interaction, comment: str = None):
-
+async def ohayou(ctx: I.discord.Interaction, comment: str = None):
+  await ctx.response.defer(thinking=True)
+  
   weekjp = ["月", "火", "水", "木", "金", "土", "日"]
   JST = I.timezone(I.timedelta(hours=+9), 'JST')
   now = I.datetime.now(JST)
@@ -13,10 +13,9 @@ async def challenge(ctx: I.discord.Interaction, comment: str = None):
   nowstr = I.datetime.strftime(now, f'%m月%d日({weekday}) %H時%M分%S秒')
   E = I.discord.Embed(title="おきました", description=f"{nowstr}に起きました")
   E.set_thumbnail(url="https://cdn.discordapp.com/attachments/1071319610550399036/1167499500722597908/646_20220307080036.png")
-  if ctx.user.nick is None:
+  username = ctx.user.nick
+  if username is None:
     username = ctx.user.name
-  else:
-    username = ctx.user.nick
   E.set_author(name=username, icon_url=ctx.user.display_avatar.url)
   if comment is not None:
     E.add_field(name="ひとこと", value=comment)
@@ -27,4 +26,4 @@ async def challenge(ctx: I.discord.Interaction, comment: str = None):
   #   ohayou_count = _v[ohayou_count] + 1
   # 書き込み #
   #####################
-  await ctx.response.send_message(embed=E)
+  await ctx.followup.send(embed=E)
