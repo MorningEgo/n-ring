7de0766e57efdfa97172b21c0648fba08c75ee25
MorningEgo <101182056+MorningEgo@users.noreply.github.com>
2023-10-27 17:16:25 +0000

    苦

--- command_files/ohayou.py
+++ command_files/ohayou.py
@@ -1,31 +1,26 @@
 import define as I
 import locale
+
 @I.tree.command(name="おはよう", description="起きます")
 @I.discord.app_commands.guilds(I.discord.Object(id=I.guildid))
-async def challenge(ctx:I.discord.Interaction, comment:str = None):
-    
-    weekjp = ["月","火","水","木","金","土","日"]
-    JST = I.timezone(I.timedelta(hours=+9), 'JST')
-    now = I.datetime.now(JST)
-    Week = I.datetime.weekday(JST)
-    weekday = weekjp[Week]
-    nowstr = I.datetime.strptime(now, f'%B%d日({weekday}) %H時%M分%S秒')
-    E = I.discord.Embed(
-        title = "おきました",
-        description = f"{nowstr}に起きました"
-    )
-    E.set_thumbnail(url="https://cdn.discordapp.com/attachments/1071319610550399036/1167499500722597908/646_20220307080036.png")
-    
-    if comment is not None:
-        E.add_field(
-            name = "ひとこと",
-            value = comment
-        )
-    ##### user_data #####
-    # # 読み込み
-    #   with open('nring_storage/n_users/user_data.json', 'r') as __e__:
-    #        = json.load(__e__)
-    #   ohayou_count = _v[ohayou_count] + 1
-    # 書き込み #
-    #####################
-    await ctx.response.send_message(embed=E)
+async def challenge(ctx: I.discord.Interaction, comment: str = None):
+
+  weekjp = ["月", "火", "水", "木", "金", "土", "日"]
+  JST = I.timezone(I.timedelta(hours=+9), 'JST')
+  now = I.datetime.now(JST)
+  Week = I.datetime.weekday(now)
+  weekday = weekjp[Week]
+  nowstr = I.datetime.strftime(now, f'%m月%d日({weekday}) %H時%M分%S秒')
+  E = I.discord.Embed(title="おきました", description=f"{nowstr}に起きました")
+  E.set_thumbnail(url="https://cdn.discordapp.com/attachments/1071319610550399036/1167499500722597908/646_20220307080036.png")
+  E.set_author(name=ctx.user.nick, icon_url=ctx.user.display_avatar.url)
+  if comment is not None:
+    E.add_field(name="ひとこと", value=comment)
+  ##### user_data #####
+  # # 読み込み
+  #   with open('nring_storage/n_users/user_data.json', 'r') as __e__:
+  #        = json.load(__e__)
+  #   ohayou_count = _v[ohayou_count] + 1
+  # 書き込み #
+  #####################
+  await ctx.response.send_message(embed=E)
