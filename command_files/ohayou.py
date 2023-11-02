fd5de01d03a8b95b1339fa43bff26f30106b1f35
朝の無意識 / Misk <morningego@gmail.com>
2023-11-03 03:19:30 +0900

    ローカル画像の使い方間違えた

--- command_files/ohayou.py
+++ command_files/ohayou.py
@@ -5,6 +5,8 @@ import define as I
 async def ohayou(ctx:I.discord.Interaction, comment: str = None):
   await ctx.response.defer(thinking=True)
   
+  file = I.discord.File("pictures\cmd_ohayou.png", filename="image.png")
+
   weekjp = ["月", "火", "水", "木", "金", "土", "日"]
   JST = I.timezone(I.timedelta(hours=+9), 'JST')
   now = I.datetime.now(JST)
@@ -12,7 +14,7 @@ async def ohayou(ctx:I.discord.Interaction, comment: str = None):
   weekday = weekjp[Week]
   nowstr = I.datetime.strftime(now, f'%m月%d日({weekday}) %H時%M分%S秒')
   E = I.discord.Embed(title="おきました", description=f"{nowstr}に起きました")
-  E.set_thumbnail(url="pictures\cmd_ohayou.png")
+  E.set_thumbnail(url="attachment://image.png")
   username = ctx.user.nick
   if username is None:
     username = ctx.user.name
@@ -26,4 +28,4 @@ async def ohayou(ctx:I.discord.Interaction, comment: str = None):
   #   ohayou_count = _v[ohayou_count] + 1
   # 書き込み #
   #####################
-  await ctx.followup.send(embed=E)
+  await ctx.followup.send(file=file, embed=E)
